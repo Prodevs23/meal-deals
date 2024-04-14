cf4d02fa19368659a5efa384dd4f9efb4f3e6323
Prodevs23 <144585442+Prodevs23@users.noreply.github.com>
2024-04-14 08:19:42 +0200

    Delete resources directory

--- resources/views/includes/foot.blade.php
+++
@@ -1,8 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
-<footer style="background-color: aqua; text-align: center;">
-    <p>Author: Prodevs23</p>
-</footer>
-
-</body>
-</html>
--- resources/views/layouts/app.blade.php
+++
@@ -1,9 +0,0 @@
-@include('includes.head')
-@include('includes.nav')
-
-<main role="main" class="container">
-    @yield('content')
-    @yield('main_page')
-</main>
-
-@include('includes.foot')
